215ae62edc782d193500727462e944f75ade8046
Tamas Szabo <szabtam@gmail.com>
2022-05-05 07:11:43 +0300

    Updating template based on current ang/mnd

---
+++ {{cookiecutter.project_slug}}/setup_github.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+git init
+git add .
+git commit -m "Initial commit"
+git branch -M next_release
+git remote add origin git@github.com:{{ cookiecutter.github_repo  }}.git
+git push -u origin next_release
+
+git submodule add -b next_release ../trrf.git rdrf
+git ci -m 'Setup TRRF git submodule'
+git push
+
+git checkout -b cicd
+git push -u origin cicd
+
+git checkout next_release
+
